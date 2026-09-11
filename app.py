7695ede0a21b7c17e554ffd9dbd3a1347311b07e
somgiat <53362309+somgiat@users.noreply.github.com>
2026-09-11 19:46:36 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Herby V16",
+    page_icon="❤️"
+)
+
+st.title("❤️ Herby")
+
+st.subheader("Your Digital Financial Mentor")
+
+st.write("Your Future Needs You Today.")
