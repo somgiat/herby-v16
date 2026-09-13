b137f951fa8613222d21ee93debc7f47f9a888cc
somgiat <53362309+somgiat@users.noreply.github.com>
2026-09-13 21:50:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -161,16 +161,16 @@ Herby อยากเข้าใจเป้าหมาย
     dna_type = "Balanced Investor"
 
     if drawdown == "ทยอยซื้อเพิ่ม" and volatility >= 7:
-        dna_type = "🚀 Growth Hunter"
+    dna_type = "🚀 Growth Hunter"
 
     elif drawdown == "ถือไว้" and volatility >= 5:
-        dna_type = "📈 Long-Term Builder"
+    dna_type = "📈 Long-Term Builder"
 
     elif drawdown == "ขายบางส่วน":
-        dna_type = "⚖️ Balanced Investor"
+    dna_type = "⚖️ Balanced Investor"
 
     elif drawdown == "ขายทั้งหมด":
-        dna_type = "🛡️ Capital Protector"
+    dna_type = "🛡️ Capital Protector"
 
     st.success(f"❤️ ยินดีที่ได้รู้จักนะ {name}")
 
